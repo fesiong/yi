9b0d55dbc8577c2db090878fcdbcfaef3ad772e3
Sinclair <tpyzlxy@gmail.com>
2023-03-29 09:39:28 +0800

    完善大衍字段

--- dayan.go
+++ dayan.go
@@ -12,7 +12,12 @@ type DaYan struct {
 	Lucky   string
 	NvMing  string
 	Max     bool
+	Gua     string
 	SkyNine string
+	YiXiang string
+	Basis   string
+	Family  string
+	Health  string
 	Comment string
 }
 
@@ -49,8 +54,13 @@ func init() {
 			Lucky:   record[1],
 			NvMing:  record[2],
 			Max:     max,
+			Gua:     record[4],
 			SkyNine: record[5],
-			Comment: record[6],
+			YiXiang: record[6],
+			Basis:   record[7],
+			Family:  record[8],
+			Health:  record[9],
+			Comment: record[10],
 		}
 
 		daYanList[dayan.Number-1] = &dayan
@@ -62,12 +72,12 @@ func (dy DaYan) IsNotSuitableGirl() bool {
 	return dy.NvMing == "凶"
 }
 
-//IsMax 是否最大好运数
+// IsMax 是否最大好运数
 func (dy DaYan) IsMax() bool {
 	return dy.Max
 }
 
-//GetDaYan 获取大衍之数
+// GetDaYan 获取大衍之数
 func GetDaYan(idx int) DaYan {
 	if idx <= 0 {
 		panic("wrong idx")
